cc24bacbf31b519c3c65bfbdb1f330be64069959
Avi Aryan <avi.aryan123@gmail.com>
2017-02-22 10:00:21 +0530

    refactor: deleted obsolete files

--- decrypt.py
+++
@@ -1,6 +0,0 @@
-#!/Users/aviaryan/miniconda3/bin/python
-from cryptlib import get_file_list, decode, update_file, get_key
-
-flist = get_file_list()
-print(flist)
-update_file(decode, flist, get_key())
--- encrypt.py
+++
@@ -1,6 +0,0 @@
-#!/Users/aviaryan/miniconda3/bin/python
-from cryptlib import get_file_list, encode, update_file, get_key
-
-flist = get_file_list()
-print(flist)
-update_file(encode, flist, get_key())
